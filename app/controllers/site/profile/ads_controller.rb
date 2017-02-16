d44e3db1c38868002d823a31e438e926a0f11510
Joao Alberto <aquinosilva@gmail.com>
2017-02-16 19:20:44 +0000

    Editando anúncios - Parte1/2

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -1,5 +1,17 @@
 class Site::Profile::AdsController < Site::ProfileController
+	before_action :set_ad, only: [:edit]
+
 	def index
-		@ads = Ad.where(member: current_member)
+		@ads = Ad.to_the(current_member)
 	end
+
+	def edit
+		# Set ad via before_action
+	end
+
+	private
+
+		def set_ad
+			@ad = Ad.find(params[:id])
+		end
 end
