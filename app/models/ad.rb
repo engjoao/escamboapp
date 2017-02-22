ac6b4863b893dc6dba13988533d00ec830dadc77
Joao Alberto <aquinosilva@gmail.com>
2017-02-22 19:29:55 +0000

    Aula 74 -  Dealhe do anúncio

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -14,7 +14,7 @@ class Ad < ActiveRecord::Base
   scope :to_the, ->(member) { where(member: member) }
 
 	#paperclip
-	has_attached_file :picture, styles: { medium: "320x150#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
+	has_attached_file :picture, styles: { medium: "320x150#", thumb: "100x100#", large: "800x300#" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   
   #gem money-rails
