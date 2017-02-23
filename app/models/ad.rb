5cdd382bc3f1a889ffe814cc0d58da24f3c83706
Joao Alberto <aquinosilva@gmail.com>
2017-02-23 19:13:21 +0000

    Aula 78 - Permitindo Markdown nos anuncios via Callback

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,22 +1,48 @@
 class Ad < ActiveRecord::Base
 
+  # Callbacks
+  before_save :md_to_html
+
+  # Associations
   belongs_to :category
   belongs_to :member
 
-  #Validates
-  validates :title, :description, :category, presence: true
+  # Validates
+  validates :title, :description_md, :description_short, :category, presence: true
   validates :picture, :finish_date, presence: true
   validates :price, numericality: { greater_than: 0 }
 
 
-  #Scopes
+  # Scopes
   scope :descending_order, ->(quantity = 10) { limit(quantity).order(created_at: :desc) }
   scope :to_the, ->(member) { where(member: member) }
 
-	#paperclip
+	# paperclip
 	has_attached_file :picture, styles: { medium: "320x150#", thumb: "100x100#", large: "800x300#" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   
-  #gem money-rails
+  # gem money-rails
   monetize :price_cents
+
+  private
+    def md_to_html()
+      options = {
+          filter_html: true,
+          link_attributes: {
+            rel: "nofollow",
+            target: "_blank"
+          }
+      }
+
+      extensions = {
+        space_after_headers: true,
+        autolink: true
+      }
+
+      renderer = Redcarpet::Render::HTML.new(options)
+      markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+      self.description = markdown.render(self.description_md)
+    end
+
 end
