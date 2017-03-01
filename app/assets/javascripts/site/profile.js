3e9977e724e5b9dea699bac262ea93793c8f9ac6
Joao Alberto <aquinosilva@gmail.com>
2017-03-01 14:17:57 +0000

    Aula 80 - Contador de anuncios

--- app/assets/javascripts/site/profile.js
+++ app/assets/javascripts/site/profile.js
@@ -2,7 +2,7 @@
 //= require jquery-ui/i18n/datepicker-pt-BR
 //= require bootstrap-markdown
 //= require marked
-
+	
 /**
  * Brazilian Portuguese translation for bootstrap-markdown
  * Gabriel Anderson <gcacars@>
