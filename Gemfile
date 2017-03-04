079ec04130d97bddbf0497709c50fe893ec2ca0b
Joao Alberto <aquinosilva@gmail.com>
2017-03-04 18:50:16 +0000

    Aula 93 - Star Rating - Avaliacoes

--- Gemfile
+++ Gemfile
@@ -67,6 +67,8 @@ gem 'wiselinks'
 gem 'kaminari'
 # Translations for the kaminari gem
 gem 'kaminari-i18n'
+# A Ruby Gem that wraps the functionality of jQuery Raty library, and provides optional IMDB style rating.
+gem 'ratyrate'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,46 +3,51 @@ class Ad < ActiveRecord::Base
   # Constants
   QTT_PER_PAGE = 6
 
+  # RatyRate gem
+  ratyrate_rateable 'quality'
+
   # Callbacks
   before_save :md_to_html
 
   # Associations
   belongs_to :member
   belongs_to :category, counter_cache: true
-  has_many :comments  
+  has_many :comments
 
   # Validates
   validates :title, :description_md, :description_short, :category, presence: true
-  validates :picture, :finish_date, presence: true
+  validates :picture, :finish_date, presence:true
   validates :price, numericality: { greater_than: 0 }
 
-
   # Scopes
-  scope :descending_order, ->(page) { 
-    order(created_at: :desc).page(page).per(QTT_PER_PAGE) }
+  scope :descending_order, ->(page) {
+    order(created_at: :desc).page(page).per(QTT_PER_PAGE)
+  }
 
-  scope :search, ->(term) { 
-    where("lower(title) LIKE ?", "%#{term.downcase}%").page(page).per(QTT_PER_PAGE) }
-  
+  scope :search, ->(term) {
+    where("lower(title) LIKE ?", "%#{term.downcase}%").page(page).per(QTT_PER_PAGE)
+  }
+
   scope :to_the, ->(member) { where(member: member) }
   scope :by_category, ->(id) { where(category: id) }
-  
-	# paperclip
-	has_attached_file :picture, styles: { medium: "320x150#", thumb: "100x100#",
-    large: "800x300#" }, default_url: "/images/:style/missing.png"
-	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
-  
+
+
+
+  # paperclip
+  has_attached_file :picture, styles: { large: "800x300#", medium: "320x150#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+
   # gem money-rails
   monetize :price_cents
 
   private
-    def md_to_html()
+    def md_to_html
       options = {
-          filter_html: true,
-          link_attributes: {
-            rel: "nofollow",
-            target: "_blank"
-          }
+        filter_html: true,
+        link_attributes: {
+          rel: "nofollow",
+          target: "_blank"
+        }
       }
 
       extensions = {
@@ -55,5 +60,4 @@ class Ad < ActiveRecord::Base
 
       self.description = markdown.render(self.description_md)
     end
-
 end
